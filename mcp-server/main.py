2126c72e493b3511b0ef4d4605637937eb6b6d1c
Kevin Coleman <kevin.coleman@sparkstart.io>
2025-06-21 12:34:08 -0700

    first commit

---
+++ mcp-server/main.py
@@ -0,0 +1,45 @@
+import asyncio
+import os
+
+from mcp_agent.app import MCPApp
+from mcp_agent.agents.agent import Agent
+from mcp_agent.workflows.llm.augmented_llm_openai import OpenAIAugmentedLLM
+
+app = MCPApp(name="Reverb AI marketing")
+
+async def reverb_ai_mcp():
+    async with app.run() as mcp_agent_app:
+        logger = mcp_agent_app.logger
+        # This agent can read the filesystem or fetch URLs
+        finder_agent = Agent(
+            name="finder",
+            instruction="""You can edit images to """,
+            server_names=["fetch", "filesystem"], # MCP servers this Agent can use
+        )
+
+        async with finder_agent:
+            # Automatically initializes the MCP servers and adds their tools for LLM use
+            tools = await finder_agent.list_tools()
+            logger.info(f"Tools available:", data=tools)
+
+            # Attach an OpenAI LLM to the agent (defaults to GPT-4o)
+            llm = await finder_agent.attach_llm(OpenAIAugmentedLLM)
+
+            # This will perform a file lookup and read using the filesystem server
+            result = await llm.generate_str(
+                message="Show me what's in README.md verbatim"
+            )
+            logger.info(f"README.md contents: {result}")
+
+            # Uses the fetch server to fetch the content from URL
+            result = await llm.generate_str(
+                message="Print the first two paragraphs from https://www.anthropic.com/research/building-effective-agents"
+            )
+            logger.info(f"Blog intro: {result}")
+
+            # Multi-turn interactions by default
+            result = await llm.generate_str("Summarize that in a 128-char tweet")
+            logger.info(f"Tweet: {result}")
+
+if __name__ == "__main__":
+    asyncio.run(reverb_ai_mcp())
